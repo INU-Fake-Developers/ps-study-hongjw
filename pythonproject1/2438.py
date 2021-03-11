a64dbb8c6ef39e8125e1f9653f59765dc5ddd1c6
Jungwoo Hong <79641160+Martinelli-3535@users.noreply.github.com>
2021-03-11 16:38:17 +0900

    kk

---
+++ pythonproject1/2438.py
@@ -0,0 +1,5 @@
+print('n:')
+n=int(input('n:'))
+
+for i in range (1,n+1):
+    print('*' , end='')
--- pythonproject1/main.py
+++ pythonproject1/main.py
@@ -1,5 +1,4 @@
-print('시험 점수 입력:') # 시험 점수는 0이상 100이하의 정수
-a=int(input('시험 점수 :'))
+a=int(input(''))
 
 if 100 >= a >= 90:
     print('A')
@@ -15,3 +14,4 @@ elif 69 >= a >= 60:
 
 else :
     print('F')
+
