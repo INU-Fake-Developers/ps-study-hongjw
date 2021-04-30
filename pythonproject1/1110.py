455b0979efb6303a69f5e11484b2536938ac013b
Jungwoo Hong <79641160+Martinelli-3535@users.noreply.github.com>
2021-04-30 22:04:03 +0900

    4/30

--- pythonproject1/1110.py
+++ pythonproject1/1110.py
@@ -1,3 +1,27 @@
-n=int(input())
+N=int(input())
+num=N
+cnt=0
 
-while n < 10 :
+while True:
+    a= num // 10
+    b= num % 10
+    c= (a+b) % 10
+
+    num= b*10 + c
+
+    cnt +=1
+
+    if N == num:
+        break
+
+print(cnt)
+
+
+
+
+
+
+
+
+
+
---
+++ pythonproject1/2751.py
@@ -0,0 +1,14 @@
+N=int(input())
+k=[]
+for i in range(N):
+    c=int(input())
+    k.append(c)
+
+k.sort()
+
+for j in range(len(k)):
+    print(k[j])
+
+
+
+
