cd5e01e9291d54e31b9168a68f3b7c159d4bbabf
Jungwoo Hong <ghost1434@naver.com>
2022-02-21 21:12:07 +0900

    2/21 MERGE SORT

--- pythonproject1/main.py
+++ pythonproject1/main.py
@@ -1,17 +1,47 @@
-a=int(input(''))
+#병합 정렬
+#입력: 리스트 a
+#출력: 없음
 
-if 100 >= a >= 90:
-    print('A')
+def merge_sort(a):
+    n=len(a)
+    if n <= 1:
+        return
 
-elif 89 >= a >= 80:
-    print('B')
+    mid = n//2
+    g1=a[:mid]
+    g2=a[mid:]
 
-elif 79 >= a >= 70:
-    print('C')
+    merge_sort(g1)
+    merge_sort(g2)
 
-elif 69 >= a >= 60:
-    print('D')
+    i1=0
+    i2=0
+    ia=0
 
-else :
-    print('F')
+    while i1<len(g1) and i2<len(g2):
+        if g1[i1] < g2[i2]:
+            a[ia] = g1[i1]
+            i1 += 1
+            ia += 1
+
+        else:
+            a[ia] = g2[i2]
+            i2 += 1
+            ia += 1
+
+    while i1<len(g1):
+        a[ia] = g1[i1]
+        i1 += 1
+        ia += 1
+
+    while i2<len(g2):
+        a[ia] = g2[i2]
+        i2 += 1
+        ia += 1
+
+
+
+d=[6, 8, 3, 9, 10, 1, 2, 4, 7, 5]
+merge_sort(d)
+print(d)
 
